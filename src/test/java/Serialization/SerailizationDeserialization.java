7a9edddc7b976e859a1f27872eaaba5b8117abe9
Muaaz.Mohideen <muaazster@gmail.com>
2022-07-05 13:33:40 +0530

    serialization&deserialization

---
+++ src/test/java/Serialization/SerailizationDeserialization.java
@@ -0,0 +1,35 @@
+package Serialization;
+
+import java.io.*;
+
+class Test implements Serializable{
+
+    int i=10, j = 20;
+}
+
+
+
+public class SerailizationDeserialization {
+
+    public static void main (String[]args) throws IOException, ClassNotFoundException {
+
+        Test t1 = new Test();
+
+        //Serialization
+        FileOutputStream fos = new FileOutputStream("test.txt");
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
+
+        oos.writeObject(t1);
+
+
+        //Desrialization
+        FileInputStream fis = new FileInputStream("test.txt");
+        ObjectInputStream ois = new ObjectInputStream(fis);
+
+        Test t2 = (Test) ois.readObject();
+        System.out.println(t2.i + "  " +t2.j);
+
+    }
+
+
+}
